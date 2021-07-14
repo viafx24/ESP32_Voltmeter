aceb59c2918953c28f77ffd07b8c92dab2b53539
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-14 11:39:23 +0200

    average on 32 samples; code working

--- src/main.cpp
+++ src/main.cpp
@@ -17,31 +17,41 @@ const int Pin_32 = 32;
 const int Pin_35 = 35;
 const int Pin_34 = 34;
 
-int ADC_Pin_33 = 0;
-int ADC_Pin_32 = 0;
-int ADC_Pin_35 = 0;
-int ADC_Pin_34 = 0;
-
 float Voltage_Pin_33 = 0;
 float Voltage_Pin_32 = 0;
 float Voltage_Pin_35 = 0;
 float Voltage_Pin_34 = 0;
 
-float Corrected_Voltage_Pin_34;
+uint16_t ADC_Pin_33,ADC_Pin_32,ADC_Pin_35,ADC_Pin_34;
+
+uint16_t ADC_Pin_34_Array[32];
+
+uint16_t ADC_Pin_34_Average;
 
 uint16_t adc0, adc1, adc2, adc3;
 
-float voltage_0, voltage_1, voltage_2, voltage_3;
+uint16_t adc0_Corrected; 
 
-float voltage_0_bis;
+uint8_t Number_Samples = 32;
 
-float Corrected_voltage_0;
+
+
+uint16_t average (uint16_t * array, uint8_t len)  // assuming array is int.
+{
+  long sum = 0L ;  // sum will be larger than an item, long for safety.
+  for (int i = 0 ; i < len ; i++)
+    sum += array [i] ;
+  return   sum / len ;  // return an int not a float
+}
+
 
 void setup(void)
 {
 
   Serial.begin(115200);
   ads1115.begin();
+
+
 }
 
 void loop(void)
@@ -51,32 +61,23 @@ void loop(void)
     dacWrite(25, i);
 
     adc0 = ads1115.readADC_SingleEnded(0);
-    voltage_0 = ads1115.computeVolts(adc0);
-    voltage_0_bis = (adc0 * 0.1875) / 1000;
-    //Corrected_voltage_0 = (voltage_0 * (9.96 + 0.99)) / 0.99;
+    adc0_Corrected = (adc0 * (6.144/3.3) * pow(2,12)) / pow(2,15) ;
+    ADC_Pin_34 = analogRead(Pin_34);
+    //analogSetSamples(Samples);
+   for (uint8_t j= 0; j<32 ;j++)
+   {
+    ADC_Pin_34_Array[j] = analogRead(Pin_34);
+   }
 
-  //  Serial.print("ADS1115: ");
+    ADC_Pin_34_Average = average (ADC_Pin_34_Array, Number_Samples) ;
+
     Serial.print(i);
     Serial.print(",");
-    Serial.print(adc0);
-    Serial.print(",");
-    Serial.print(voltage_0, 7);
-    //Serial.print(voltage_0_bis, 7);
-    // Serial.print(",");
-    // Serial.println(Corrected_voltage_0);
-
-    ADC_Pin_34 = analogRead(Pin_34);
- //   Voltage_Pin_34 = (ADC_Pin_34 * 3.18) / 4095 + 0.081;
-    Voltage_Pin_34 = (ADC_Pin_34 * 3.3) / 4095;
-    //Corrected_Voltage_Pin_34 = (((ADC_Pin_34 * 3.3) / 4095) * (9.96 + 0.99)) / (0.99);
- //   Serial.print("ESP GPIO34: ");
-
+    Serial.print(adc0_Corrected);
     Serial.print(",");
     Serial.print(ADC_Pin_34);
     Serial.print(",");
-    Serial.println(Voltage_Pin_34,7);
-    // Serial.print(",");
-    // Serial.println(Corrected_Voltage_Pin_34);
+    Serial.println(ADC_Pin_34_Average);
 
     delay(10);
   }
