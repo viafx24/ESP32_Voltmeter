7614ae484c45794d8d54ebf169a5f6e0c4cace20
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-14 14:41:05 +0200

    working 8 channels comparison

--- src/main.cpp
+++ src/main.cpp
@@ -17,41 +17,34 @@ const int Pin_32 = 32;
 const int Pin_35 = 35;
 const int Pin_34 = 34;
 
-float Voltage_Pin_33 = 0;
-float Voltage_Pin_32 = 0;
-float Voltage_Pin_35 = 0;
-float Voltage_Pin_34 = 0;
+const uint8_t Number_Samples = 32;
 
-uint16_t ADC_Pin_33,ADC_Pin_32,ADC_Pin_35,ADC_Pin_34;
+uint16_t ADC_Pin_33, ADC_Pin_32, ADC_Pin_35, ADC_Pin_34;
 
-uint16_t ADC_Pin_34_Array[32];
+uint16_t ADC_Pin_33_Array[Number_Samples];
+uint16_t ADC_Pin_32_Array[Number_Samples];
+uint16_t ADC_Pin_35_Array[Number_Samples];
+uint16_t ADC_Pin_34_Array[Number_Samples];
 
-uint16_t ADC_Pin_34_Average;
+uint16_t ADC_Pin_33_Average, ADC_Pin_32_Average, ADC_Pin_35_Average, ADC_Pin_34_Average;
 
 uint16_t adc0, adc1, adc2, adc3;
 
-uint16_t adc0_Corrected; 
+uint16_t adc0_Corrected, adc1_Corrected, adc2_Corrected, adc3_Corrected;
 
-uint8_t Number_Samples = 32;
-
-
-
-uint16_t average (uint16_t * array, uint8_t len)  // assuming array is int.
+uint16_t average(uint16_t *array, uint8_t len) // assuming array is int.
 {
-  long sum = 0L ;  // sum will be larger than an item, long for safety.
-  for (int i = 0 ; i < len ; i++)
-    sum += array [i] ;
-  return   sum / len ;  // return an int not a float
+  long sum = 0L; // sum will be larger than an item, long for safety.
+  for (int i = 0; i < len; i++)
+    sum += array[i];
+  return sum / len; // return an int not a float
 }
-
 
 void setup(void)
 {
 
   Serial.begin(115200);
   ads1115.begin();
-
-
 }
 
 void loop(void)
@@ -60,25 +53,44 @@ void loop(void)
   {
     dacWrite(25, i);
 
-    adc0 = ads1115.readADC_SingleEnded(0);
-    adc0_Corrected = (adc0 * (6.144/3.3) * pow(2,12)) / pow(2,15) ;
-    ADC_Pin_34 = analogRead(Pin_34);
-    //analogSetSamples(Samples);
-   for (uint8_t j= 0; j<32 ;j++)
-   {
-    ADC_Pin_34_Array[j] = analogRead(Pin_34);
-   }
+    adc0_Corrected = (ads1115.readADC_SingleEnded(0) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
+    adc1_Corrected = (ads1115.readADC_SingleEnded(1) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
+    adc2_Corrected = (ads1115.readADC_SingleEnded(2) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
+    adc3_Corrected = (ads1115.readADC_SingleEnded(3) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
 
-    ADC_Pin_34_Average = average (ADC_Pin_34_Array, Number_Samples) ;
+    for (uint8_t j = 0; j < 32; j++)
+    {
+      ADC_Pin_33_Array[j] = analogRead(Pin_33);
+      ADC_Pin_32_Array[j] = analogRead(Pin_32);
+      ADC_Pin_35_Array[j] = analogRead(Pin_35);
+      ADC_Pin_34_Array[j] = analogRead(Pin_34);
+    }
+
+    ADC_Pin_33_Average = average(ADC_Pin_33_Array, Number_Samples);
+    ADC_Pin_32_Average = average(ADC_Pin_32_Array, Number_Samples);
+    ADC_Pin_35_Average = average(ADC_Pin_35_Array, Number_Samples);
+    ADC_Pin_34_Average = average(ADC_Pin_34_Array, Number_Samples);
 
     Serial.print(i);
     Serial.print(",");
     Serial.print(adc0_Corrected);
     Serial.print(",");
-    Serial.print(ADC_Pin_34);
+    Serial.print(adc1_Corrected);
+    Serial.print(",");
+    Serial.print(adc2_Corrected);
+    Serial.print(",");
+    Serial.print(adc3_Corrected);
+    Serial.print(",");
+    Serial.print(ADC_Pin_33_Average);
+    Serial.print(",");
+    Serial.print(ADC_Pin_32_Average);
+    Serial.print(",");
+    Serial.print(ADC_Pin_35_Average);
     Serial.print(",");
     Serial.println(ADC_Pin_34_Average);
 
+
     delay(10);
+
   }
 }
