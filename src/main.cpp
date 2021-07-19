9381875a2fe08565c6aa59fce5abfef2c5b7acd1
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-19 17:41:58 +0200

    first write

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,6 @@ const int Pin_34 = 34;
 
 const uint8_t Number_Samples = 255;
 
-uint16_t ADC_Pin_33, ADC_Pin_32, ADC_Pin_35, ADC_Pin_34;
-
 uint16_t ADC_Pin_33_Array[Number_Samples];
 uint16_t ADC_Pin_32_Array[Number_Samples];
 uint16_t ADC_Pin_35_Array[Number_Samples];
@@ -30,11 +28,15 @@ uint16_t ADC_Pin_34_Array[Number_Samples];
 
 uint16_t ADC_Pin_33_Average, ADC_Pin_32_Average, ADC_Pin_35_Average, ADC_Pin_34_Average;
 
-uint16_t adc0, adc1, adc2, adc3;
-
 uint16_t adc0_Corrected, adc1_Corrected, adc2_Corrected, adc3_Corrected;
 
-float Voltage_Bridge_ADC0, Voltage_Bridge_ADC_Pin_34, Corrected_Voltage_ADC0, Corrected_Voltage_ADC_Pin_34;
+float Voltage_Bridge_ADC0, Voltage_Bridge_ADC1;
+float Corrected_Voltage_ADC0, Corrected_Voltage_ADC1;
+float Voltage_Bridge_ADC_Pin_34, Voltage_Bridge_ADC_Pin_35;
+float Corrected_Voltage_ADC_Pin_34, Corrected_Voltage_ADC_Pin_35;
+
+float Current_ADS1115;
+float Current_ADC_ESP32;
 
 // parameter for retrieving result from table
 
@@ -90,46 +92,64 @@ void setup(void)
 
 void loop(void)
 {
-  for (uint8_t i = 0; i < 256; i++)
+
+  adc0_Corrected = (ads1115.readADC_SingleEnded(0) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
+  adc1_Corrected = (ads1115.readADC_SingleEnded(1) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
+
+  for (uint8_t j = 0; j < Number_Samples; j++) // 32 à la base semblait suffisant.
   {
-    dacWrite(25, i);
-
-    adc0_Corrected = (ads1115.readADC_SingleEnded(0) * (6.144 / 3.3) * pow(2, 12)) / pow(2, 15);
-
-    for (uint8_t j = 0; j < Number_Samples; j++) // 32 à la base semblait suffisant.
-    {
-      ADC_Pin_34_Array[j] = analogRead(Pin_34);
-    }
-
-    ADC_Pin_34_Average = average(ADC_Pin_34_Array, Number_Samples);
-
-    Voltage_Bridge_ADC0 = adc0_Corrected * 3.3 / 4096;
-    Voltage_Bridge_ADC_Pin_34 = ADC_Pin_34_Average * 3.3 / 4096;
-
-    // Corrected_Voltage_ADC0 = (Voltage_Bridge_ADC0 * (99100 + 9960)) / 9960;
-    // Corrected_Voltage_ADC_Pin_34 = (Voltage_Bridge_ADC_Pin_34 * (99100 + 9960)) / 9960;
-
-    Serial.print(i + 1); // adapted for matlab indexing
-    Serial.print(",");
-    // Serial.print(adc0_Corrected);
-    // Serial.print(",");
-    // Serial.print(ADC_Pin_34_Average);
-    // Serial.print(",");
-
-    // true value of voltage with ads1115 channel zero
-    Serial.print(Voltage_Bridge_ADC0, 4);
-    Serial.print(",");
-
-    // false value of ADC gpio 34 without correction
-    Serial.print(Voltage_Bridge_ADC_Pin_34, 4);
-    Serial.print(",");
-
-    // corrected data for GPIO_34 (coming from file data)
-    // my guess is that (1 /ads1115) and (3/corrected voltage GPIO34) should be very close
-    // and thus no need to use ads1115 anymore.
-
-    Serial.println(MyADS1115array[ADC_Pin_34_Average], 4);
-
-    delay(10);
+    ADC_Pin_34_Array[j] = analogRead(Pin_34);
+    ADC_Pin_35_Array[j] = analogRead(Pin_35);
   }
+
+  ADC_Pin_34_Average = average(ADC_Pin_34_Array, Number_Samples);
+  ADC_Pin_35_Average = average(ADC_Pin_35_Array, Number_Samples);
+
+  Voltage_Bridge_ADC0 = adc0_Corrected * 3.3 / 4096;
+  Voltage_Bridge_ADC1 = adc1_Corrected * 3.3 / 4096;
+
+  Voltage_Bridge_ADC_Pin_35 = MyADS1115array[ADC_Pin_35_Average];
+  Voltage_Bridge_ADC_Pin_34 = MyADS1115array[ADC_Pin_34_Average];
+
+  Corrected_Voltage_ADC0 = (Voltage_Bridge_ADC0 * (97700 + 9960)) / 9960;
+  Corrected_Voltage_ADC1 = (Voltage_Bridge_ADC0 * (97700 + 9960)) / 9960;
+
+  Corrected_Voltage_ADC_Pin_34 = (Voltage_Bridge_ADC_Pin_34 * (97700 + 9960)) / 9960;
+  Corrected_Voltage_ADC_Pin_35 = (Voltage_Bridge_ADC_Pin_34 * (97700 + 9960)) / 9960;
+
+  Current_ADS1115 = (Corrected_Voltage_ADC0 - Corrected_Voltage_ADC1) / 0.1;
+
+  Current_ADC_ESP32 = (Corrected_Voltage_ADC_Pin_34 - Corrected_Voltage_ADC_Pin_35) / 0.1;
+
+  // Voltage at the bridge
+
+  Serial.print(Voltage_Bridge_ADC0, 4);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC1, 4);
+  Serial.print(",");
+
+  Serial.print(Voltage_Bridge_ADC_Pin_34, 4);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC_Pin_35, 4);
+  Serial.print(",");
+
+  // Voltage corrected by approx factor 10
+
+  Serial.print(Corrected_Voltage_ADC0, 4);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC1, 4);
+  Serial.print(",");
+
+  Serial.print(Corrected_Voltage_ADC_Pin_34, 4);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC_Pin_35, 4);
+  Serial.print(",");
+
+  // Current computed  high side through the shunt 0.1 ohm
+
+  Serial.print(Current_ADS1115, 4);
+  Serial.print(",");
+  Serial.println(Current_ADC_ESP32, 4);
+
+  delay(10);
 }
