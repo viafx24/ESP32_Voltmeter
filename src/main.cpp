1d8f0e5e81e6d636d23128ffe85321418b8a330d
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-20 12:35:22 +0200

    before comment all the useless code

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,10 @@
 
 #include <Arduino.h>
-// #include <Adafruit_ADS1X15.h>
 
-// #include <WiFi.h>
-// #include <AsyncTCP.h>
-// #include <ESPAsyncWebServer.h>
-// #include <AsyncElegantOTA.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
-// #include <Adafruit_I2CDevice.h> // Guillaume add
-// #include "SPIFFS.h"
-
-
-const int Pin_33 = 33;
-const int Pin_32 = 32;
-const int Pin_35 = 35;
 const int Pin_34 = 34;
-
-
-
-// uint16_t ADC_Pin_33_Array[Number_Samples];
-// uint16_t ADC_Pin_32_Array[Number_Samples];
-// uint16_t ADC_Pin_35_Array[Number_Samples];
-
 unsigned long Time_1;
 unsigned long Time_2;
-const long Number_Samples= 30000;
+const uint16_t Number_Samples= 30000;
 uint16_t ADC_Pin_34_Array[Number_Samples];
 
 void setup(void)
@@ -37,14 +16,20 @@ void setup(void)
 
 void loop(void)
 {
+
 Time_1 =millis();
-  for (long k = 0; k < Number_Samples; k++) // 32 à la base semblait suffisant.
+
+  for (uint16_t k = 0; k < Number_Samples; k++) 
   {
 
    ADC_Pin_34_Array[k]=analogRead(Pin_34);
-  
+
   }
+
 Time_2 =millis();
+
 Serial.println(Time_2 - Time_1);
+
 delay(1000);
+
 }
