c419df719a5cdf3536c0d837f165d0731528a5e8
Guillaume Baptist <guillaume.baptist@free.fr>
2021-07-21 19:53:56 +0200

    still an error in the code but mainly 10 channel added

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,19 @@ const int Pin_33 = 33; //adc1
 const int Pin_32 = 32; //adc1
 const int Pin_35 = 35; //adc1
 const int Pin_34 = 34; //adc1
-const int Pin_39 = 39; //adc1 / SN
+const int Pin_39 = 39; //adc1 /SN
 const int Pin_36 = 36; //adc1 /SP
 
 const uint16_t Number_Samples_ADC = 128;
 const uint16_t Number_Samples_Current = 150;
 
-R1=
-R2=
-R_Shunt
+uint16_t R1 = 97700;
+uint16_t R2 = 9960;
 
+float R_Shunt_1 = 0.1;
+float R_Shunt_2 = 0.1;
+float R_Shunt_3 = 0.1;
+float R_Shunt_4 = 0.1;
 
 Statistic ADC_Pin_33;
 Statistic ADC_Pin_32;
@@ -63,6 +66,14 @@ float Corrected_Voltage_ADC_Pin_36;
 float Current_ADS1115_Average;
 float Current_ADC_ESP32_Average;
 
+float Current_ADC_0_1_High_Side;
+float Current_ADC_2_3_High_Side;
+
+//float Current_ADC_0_Low_Side  ;
+//float Current_ADC_1_Low_Side  ;
+//float Current_ADC_2_Low_Side  ;
+//float Current_ADC_3_Low_Side  ;
+
 // parameter for retrieving result from SPIFF file
 
 String Line;
@@ -77,7 +88,7 @@ void setup(void)
   ads1115.begin();
 
   // reading in data file (SPIFFS) to get all the true value of voltage for ADC integer
-  // between 0 and 4095.
+  // between 0 and 4095. only work for adc1; adc2 should require different Lookup table.
 
   if (!SPIFFS.begin())
   {
@@ -97,12 +108,13 @@ void setup(void)
     {
 
       String line = f.readStringUntil(',');
-      //Voltage = line.toFloat();
       MyADS1115array[Count] = line.toFloat();
       Count++;
     }
   }
   f.close();
+
+// clear content of "array like" Statistic object to be sure that they are empty at the beginning.
 
   ADC_Pin_33.clear();
   ADC_Pin_32.clear();
@@ -148,65 +160,108 @@ void loop(void)
     Voltage_Bridge_ADC_Pin_36.add(MyADS1115array[uint16_t(ADC_Pin_36.average())]);
   }
 
-  Voltage_Bridge_ADC0 = (ads1115.readADC_SingleEnded(0) * 6.144) / 2 ^ 15;
-  Voltage_Bridge_ADC1 = (ads1115.readADC_SingleEnded(1) * 6.144) / 2 ^ 15;
-  Voltage_Bridge_ADC2 = (ads1115.readADC_SingleEnded(2) * 6.144) / 2 ^ 15;
-  Voltage_Bridge_ADC3 = (ads1115.readADC_SingleEnded(3) * 6.144) / 2 ^ 15;
+  Voltage_Bridge_ADC0 = (ads1115.readADC_SingleEnded(0) * 6.144) / 2^15;
+  Voltage_Bridge_ADC1 = (ads1115.readADC_SingleEnded(1) * 6.144) / 2^15;
+  Voltage_Bridge_ADC2 = (ads1115.readADC_SingleEnded(2) * 6.144) / 2^15;
+  Voltage_Bridge_ADC3 = (ads1115.readADC_SingleEnded(3) * 6.144) / 2^15;
+
+  Corrected_Voltage_ADC_Pin_33 = (Voltage_Bridge_ADC_Pin_33.average() * (R1 + R2)) / R2;
+  Corrected_Voltage_ADC_Pin_32 = (Voltage_Bridge_ADC_Pin_32.average() * (R1 + R2)) / R2;
+  Corrected_Voltage_ADC_Pin_35 = (Voltage_Bridge_ADC_Pin_35.average() * (R1 + R2)) / R2;
+  Corrected_Voltage_ADC_Pin_34 = (Voltage_Bridge_ADC_Pin_34.average() * (R1 + R2)) / R2;
+  Corrected_Voltage_ADC_Pin_39 = (Voltage_Bridge_ADC_Pin_39.average() * (R1 + R2)) / R2;
+  Corrected_Voltage_ADC_Pin_36 = (Voltage_Bridge_ADC_Pin_36.average() * (R1 + R2)) / R2;
 
   Corrected_Voltage_ADC0 = (Voltage_Bridge_ADC0 * (R1 + R2)) / R2;
   Corrected_Voltage_ADC1 = (Voltage_Bridge_ADC1 * (R1 + R2)) / R2;
   Corrected_Voltage_ADC2 = (Voltage_Bridge_ADC2 * (R1 + R2)) / R2;
   Corrected_Voltage_ADC3 = (Voltage_Bridge_ADC3 * (R1 + R2)) / R2;
-  
 
-  Corrected_Voltage_ADC_Pin_34 = (Voltage_Bridge_ADC_Pin_34 * (R1 + R2)) / R2;
-  Corrected_Voltage_ADC_Pin_35 = (Voltage_Bridge_ADC_Pin_35 * (R1 + R2)) / R2;
+  // differential High side measure of current
 
-  Current_ADS1115_Average = (Corrected_Voltage_ADC0 - Corrected_Voltage_ADC1) / R_Shunt;
-  Current_ADC_ESP32_Average = (Corrected_Voltage_ADC_Pin_34 - Corrected_Voltage_ADC_Pin_35) / R_Shunt;
+  Current_ADC_0_1_High_Side = (Corrected_Voltage_ADC0 - Corrected_Voltage_ADC1) / R_Shunt_1;
+  Current_ADC_2_3_High_Side = (Corrected_Voltage_ADC2 - Corrected_Voltage_ADC3) / R_Shunt_2;
 
-  // Voltage at the bridge
+  // Keeping the other possibility to work with 4 current sensing channels on low-side
 
-  Serial.print(Voltage_Bridge_ADC0.average(), 6);
+  // Current_ADC_0_Low_Side = (Corrected_Voltage_ADC0) / R_Shunt_1
+  // Current_ADC_1_Low_Side = (Corrected_Voltage_ADC1) / R_Shunt_2
+  // Current_ADC_2_Low_Side = (Corrected_Voltage_ADC2) / R_Shunt_3
+  // Current_ADC_3_Low_Side = (Corrected_Voltage_ADC3) / R_Shunt_4
+
+
+  // Voltage at the bridge : 10 channels
+
+  Serial.print(Voltage_Bridge_ADC0, 6);
   Serial.print(",");
-  Serial.print(Voltage_Bridge_ADC1.average(), 6);
+  Serial.print(Voltage_Bridge_ADC1, 6);
   Serial.print(",");
-  Serial.print(Voltage_Bridge_ADC0.pop_stdev() * 1000, 6);
+  Serial.print(Voltage_Bridge_ADC2, 6);
   Serial.print(",");
-
-  Serial.print(Voltage_Bridge_ADC_Pin_34.average(), 6);
+  Serial.print(Voltage_Bridge_ADC3, 6);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC_Pin_33.average(), 6);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC_Pin_32.average(), 6);
   Serial.print(",");
   Serial.print(Voltage_Bridge_ADC_Pin_35.average(), 6);
   Serial.print(",");
-  Serial.print(Voltage_Bridge_ADC_Pin_34.pop_stdev() * 1000, 6);
+  Serial.print(Voltage_Bridge_ADC_Pin_34.average(), 6);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC_Pin_39.average(), 6);
+  Serial.print(",");
+  Serial.print(Voltage_Bridge_ADC_Pin_36.average(), 6);
   Serial.print(",");
 
-  // Voltage corrected by approx factor 10
+  // Desired Voltage (corrected by approx factor 10 if R2=10*R1) allowing measurement
+  // until approx 25V securely (security margin to 33V)
 
   Serial.print(Corrected_Voltage_ADC0, 6);
   Serial.print(",");
   Serial.print(Corrected_Voltage_ADC1, 6);
   Serial.print(",");
-
-  Serial.print(Corrected_Voltage_ADC_Pin_34, 6);
+  Serial.print(Corrected_Voltage_ADC2, 6);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC3, 6);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC_Pin_33, 6);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC_Pin_32, 6);
   Serial.print(",");
   Serial.print(Corrected_Voltage_ADC_Pin_35, 6);
   Serial.print(",");
-
-  // Current computed  high side through the shunt 0.1 ohm
-
-  Serial.print(Current_ADS1115_Average, 6);
+  Serial.print(Corrected_Voltage_ADC_Pin_34, 6);
   Serial.print(",");
-  Serial.println(Current_ADC_ESP32_Average, 6);
+  Serial.print(Corrected_Voltage_ADC_Pin_39, 6);
+  Serial.print(",");
+  Serial.print(Corrected_Voltage_ADC_Pin_36, 6);
+  Serial.print(",");
 
-  Voltage_Bridge_ADC_Pin_34.clear();
+  // Current computed  function of high side or low side
+  // keep in mind that current maybe measured also with GPIO of ADS1115 but less accurately
+  // only on high side and with a strong sample number to get an average allowing good approximation.
+
+// commented but in case of want to use more channel with low side sensing.
+
+  // Serial.print(Current_ADC_0_Low_Side, 6);
+  // Serial.print(",");
+  // Serial.println(Current_ADC_1_Low_Side, 6);
+  // Serial.print(",");
+  // Serial.print(Current_ADC_2_Low_Side, 6);
+  // Serial.print(",");
+  // Serial.print(Current_ADC_3_Low_Side, 6);
+
+  Serial.print(Current_ADC_0_1_High_Side, 6);
+  Serial.print(",");
+  Serial.println(Current_ADC_0_1_High_Side, 6);
+
+
+// clear content of "array" used by Statistics objects at each iteration
+
+  Voltage_Bridge_ADC_Pin_33.clear();
+  Voltage_Bridge_ADC_Pin_32.clear();
   Voltage_Bridge_ADC_Pin_35.clear();
-  Voltage_Bridge_ADC0.clear();
-  Voltage_Bridge_ADC1.clear();
+  Voltage_Bridge_ADC_Pin_34.clear();
+  Voltage_Bridge_ADC_Pin_39.clear();
+  Voltage_Bridge_ADC_Pin_36.clear();
 }
-
-// adc0_Corrected = (ads1115.readADC_SingleEnded(0) * (6.144 / 3.3) ;
-// adc1_Corrected = (ads1115.readADC_SingleEnded(1) * (6.144 / 3.3) ;
-
-// Voltage_Bridge_ADC0.add(adc0_Corrected * 3.3 / 4096);
-// Voltage_Bridge_ADC1.add(adc1_Corrected * 3.3 / 4096);
